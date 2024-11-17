9257a2aa95ab488911daeca1cf85b651c081afb6
RiabushenkoA <ariabushenko@blackbird.io>
2024-11-17 21:33:20 +0200

    changes 4

--- Apps.X.AI/Models/Request/ChatCompletionRequest.cs
+++ Apps.X.AI/Models/Request/ChatCompletionRequest.cs
@@ -14,7 +14,7 @@ namespace Apps.X.AI.Models.Request
 
         [Display("Messages", Description = "A list of messages for the chat conversation.")]
         [JsonProperty("messages")]
-        public List<Message> Messages { get; set; }
+        public List<string> Messages { get; set; }
 
         [Display("Max tokens", Description = "The maximum number of tokens to generate before stopping.")]
         [JsonProperty("max_tokens")]
